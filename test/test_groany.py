e7590ab544f9916b52eff1119f7032b5ad37e1f8
Monarch Wadia <monarchwadia@gmail.com>
2022-11-25 07:56:31 -0500

    Added unit tests.

--- test/test_groany.py
+++ test/test_groany.py
@@ -3,9 +3,17 @@ from groany import groany
 from tempfile import mkdtemp
 from pytest import fixture
 from pathlib import Path
-from io import BytesIO, StringIO
+from io import BytesIO, StringIO, TextIOWrapper
 from groany.scripts import cli
-from typing import Any
+from typing import Any, Callable
+
+class MockBuffer:
+  def __init__(self, data: str):
+    self.data = data
+
+  def readline(self) -> str:
+    return self.data
+
 @fixture(autouse=True)
 def setup():
   # setup
@@ -57,6 +65,89 @@ def test_funny_exits_program():
   assert isinstance(the_mock, StringIO)
   assert the_mock.getvalue() == "(Cmd) Goodbye!\n"
 
+@patch('groany.api.search')
+def test_uniqueness(search: MagicMock):
+  api_return_value = {
+    'results':[
+      {
+        'joke': 'first joke',
+        'id': 'test_id_1'
+      },
+      {
+        'joke': 'second joke',
+        'id': 'test_id_2'
+      }
+    ],
+    'total_jokes': 3,
+    'total_pages': 1,
+    'current_page': 1,
+    'next_page': 1,
+    'previous_page': 1,
+    'search_term': 'test',
+    'status': 200,
+    'limit': 30
+  }
+
+  search.return_value = api_return_value
+
+  with patch("sys.stdin", StringIO("test\ntest\ntest\nfunny\n")), patch("sys.stdout", new_callable=StringIO) as mocked_out:
+    cli.exec()
+
+  the_mock: Any = mocked_out # Ugly hack to get Pylance to stop complaining about types
+  assert isinstance(the_mock, StringIO)
+  assert the_mock.getvalue() == "(Cmd) first joke\n(Cmd) second joke\n(Cmd) No jokes found.\n(Cmd) Goodbye!\n"
   
 
+@patch('groany.api.search')
+def test_uniqueness_across_invocations(search: MagicMock):
+  api_return_value = {
+    'results':[
+      {
+        'joke': 'first joke',
+        'id': 'test_id_1'
+      },
+      {
+        'joke': 'second joke',
+        'id': 'test_id_2'
+      }
+    ],
+    'total_jokes': 3,
+    'total_pages': 1,
+    'current_page': 1,
+    'next_page': 1,
+    'previous_page': 1,
+    'search_term': 'test',
+    'status': 200,
+    'limit': 30
+  }
 
+  search.return_value = api_return_value
+
+  # First invocation
+
+  with patch("sys.stdin", StringIO("test\nfunny\n")), patch("sys.stdout", new_callable=StringIO) as mocked_out:
+    cli.exec()
+
+  the_mock: Any = mocked_out # Ugly hack to get Pylance to stop complaining about types
+  assert isinstance(the_mock, StringIO)
+  assert the_mock.getvalue() == "(Cmd) first joke\n(Cmd) Goodbye!\n"
+
+  # 2nd invocation
+
+  with patch("sys.stdin", StringIO("test\nfunny\n")), patch("sys.stdout", new_callable=StringIO) as mocked_out:
+    cli.exec()
+
+  the_mock: Any = mocked_out # Ugly hack to get Pylance to stop complaining about types
+  assert isinstance(the_mock, StringIO)
+  assert the_mock.getvalue() == "(Cmd) second joke\n(Cmd) Goodbye!\n"
+
+  # 3rd invocation
+
+  with patch("sys.stdin", StringIO("test\nfunny\n")), patch("sys.stdout", new_callable=StringIO) as mocked_out:
+    cli.exec()
+
+  the_mock: Any = mocked_out # Ugly hack to get Pylance to stop complaining about types
+  assert isinstance(the_mock, StringIO)
+
+  assert the_mock.getvalue() == "(Cmd) No jokes found.\n(Cmd) Goodbye!\n"
+  
